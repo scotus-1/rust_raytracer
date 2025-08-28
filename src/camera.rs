2d57b6443fd722470320fd7dc46039715ef37a0f
Titus Nguyen <titus.tnguyen17@gmail.com>
2025-08-27 18:53:20 -0700

    lets move the camera bitch

--- src/camera.rs
+++ src/camera.rs
@@ -1,19 +1,12 @@
 use std::{f32::INFINITY, io, io::Write};
 
-use bevy::math::{vec3, Dir3, Ray3d, Vec3};
+use bevy::{math::{Dir3, Ray3d, Vec3}};
 use crate::{color::{write_color, Color}, hittable::Hittable, interval::Interval, util::{degs_to_rads, random_sample_sq}, Point3};
 
 
 const ASPECT_RATIO_DEF: f32 = 16.0 / 9.0;
-
-// camera properties
-const FOCAL_LENGTH: f32 = 1.0;
-const CAMERA_CENTER: Point3 = Point3::new(0.0, 0.0, 0.0);
-
-// Image dimensions in INT
 const IMAGE_WIDTH_DEF: u32 = 800;
-const VIEWPORT_HEIGHT: f32 = 2.0;
-
+const CENTER_DEF: Point3 = Point3::ZERO;
 const SAMPLES_PER_PIXEL_DEF: u32 = 10;
 const MAX_CHILD_RAYS_DEF: u32 = 10;
 const VFOV_DEF: u32 = 90;
@@ -28,7 +21,13 @@ pub struct Camera {
     pub pixel_sample_scale: f32,
     pub max_child_rays: u32,
     pub vfov: u32,
-    center: Point3,
+    pub center: Point3,
+    pub look_at_point: Point3,
+    focal_length: f32,
+    pub vup: Dir3,
+    u: Dir3,
+    v: Dir3,
+    w: Dir3,
     pixel00_loc: Point3,
     pixel_delta_u: Vec3,
     pixel_delta_v: Vec3
@@ -64,32 +63,44 @@ impl Camera {
 
     pub fn new() -> Self {
         Camera { aspect_ratio: ASPECT_RATIO_DEF, image_width: IMAGE_WIDTH_DEF, image_height: 0, samples_per_pixel: SAMPLES_PER_PIXEL_DEF, pixel_sample_scale: 1.0/SAMPLES_PER_PIXEL_DEF as f32,
-                max_child_rays: MAX_CHILD_RAYS_DEF, vfov: VFOV_DEF, center: Point3::ZERO, pixel00_loc: Point3::ZERO, pixel_delta_u: Vec3::ZERO, pixel_delta_v: Vec3::ZERO }
+                max_child_rays: MAX_CHILD_RAYS_DEF, vfov: VFOV_DEF, center: CENTER_DEF, pixel00_loc: Point3::ZERO, pixel_delta_u: Vec3::ZERO, pixel_delta_v: Vec3::ZERO,
+                look_at_point: Point3::new(0.0,0.0,-1.0),
+                focal_length: 1.0, vup: Dir3::Y,
+                u: Dir3::X, v: Dir3::Y, w: Dir3::Z }
     }
 
     // init recalculates all basis vectors important to rendering based off any changes made explicitly to cut time
     fn init(&mut self) {
+        // recalc samples per pixel so everything isnt blown to bits
+        self.pixel_sample_scale = 1.0/self.samples_per_pixel as f32;
         // const image_height: u32 = if image_width < 1 {1} else {image_height};
         // image width & height are rounded to integers for pixel values
         self.image_height = (self.image_width as f32/self.aspect_ratio) as u32;
 
         // viewport properties
+        self.focal_length = (self.center - self.look_at_point).length();
         let theta = degs_to_rads(self.vfov as f32);
         let h = (theta / 2.0).tan();
 
-        let viewport_height = 2.0 * h * FOCAL_LENGTH;
+        // calculate basis vectors
+        // vup could quite literally be anything its just that you want the rotation of the image to be justifiable
+        self.w = Dir3::new(self.center - self.look_at_point).unwrap();
+        self.u = Dir3::new(self.vup.cross(self.w.as_vec3())).unwrap();
+        self.v = Dir3::new(self.w.cross(self.u.as_vec3())).unwrap();
+        
+        let viewport_height = 2.0 * h * self.focal_length;
         let viewport_width: f32 = viewport_height * (self.image_width as f32 / self.image_height as f32);
         // basically the image height and width step insure integer pixel values which are then coverted
         // back into a REALER TRUER USABLE ratio, aka the viewport height and width
-
+        
         // viewport vectors and delta vectors, basically from (0,0) to whatever +u,+v
-        let viewport_u: Vec3 = vec3(viewport_width, 0.0, 0.0);
-        let viewport_v: Vec3 = vec3(0.0, -viewport_height, 0.0);
+        let viewport_u: Vec3 = viewport_width * self.u;
+        let viewport_v: Vec3 = viewport_height * -self.v;
         self.pixel_delta_u = viewport_u / self.image_width as f32;
         self.pixel_delta_v = viewport_v / self.image_height as f32;
         
         // note: foward is in the -z direction, which is why the focal length vec is subbed from cam_center
-        let viewport_upper_left = CAMERA_CENTER - vec3(0.0,0.0,FOCAL_LENGTH) - viewport_u/2f32 - viewport_v/2f32;
+        let viewport_upper_left = self.center - (self.focal_length * self.w) - viewport_u/2.0 - viewport_v/2.0;
         // note: pixel00 is not where (0,0) on the u,v coords, pixel00 is the first middle of the first square pixel
         self.pixel00_loc = viewport_upper_left + 0.5*(self.pixel_delta_u + self.pixel_delta_v);
 
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::{f32::consts::PI, rc::Rc};
-use bevy::{math::Vec3};
+use bevy::math::{Dir3, Vec3};
 
 use crate::{camera::Camera, color::Color, hittable_list::HittableList, material::{DielectricMaterial, LambertianMaterial, MetalMaterial}, sphere::sphere};
 pub type Point3 = Vec3;
@@ -17,32 +17,35 @@ mod material;
 
 fn main() {
     let mut world: HittableList = HittableList::hittable_list(None);
-    // let mat_ground: Rc<LambertianMaterial> = Rc::new(LambertianMaterial{albedo: Color::new(0.8, 0.8, 0.0)});
-    // let mat_center: Rc<LambertianMaterial> = Rc::new(LambertianMaterial{albedo: Color::new(0.1, 0.2, 0.5)});
-    // let mat_left: Rc<DielectricMaterial> = Rc::new(DielectricMaterial::new(1.50));
-    // let mat_bubble: Rc<DielectricMaterial> = Rc::new(DielectricMaterial::new(1.00/1.50));
-    // let mat_right: Rc<MetalMaterial> = Rc::new(MetalMaterial::new(Color::new(0.8, 0.6, 0.2), 1.0 ));
+    let mat_ground: Rc<LambertianMaterial> = Rc::new(LambertianMaterial::new(Color::new(0.8, 0.8, 0.0)));
+    let mat_center: Rc<LambertianMaterial> = Rc::new(LambertianMaterial::new(Color::new(0.1, 0.2, 0.5)));
+    let mat_left: Rc<DielectricMaterial> = Rc::new(DielectricMaterial::new(1.50));
+    let mat_bubble: Rc<DielectricMaterial> = Rc::new(DielectricMaterial::new(1.00/1.50));
+    let mat_right: Rc<MetalMaterial> = Rc::new(MetalMaterial::new(Color::new(0.8, 0.6, 0.2), 0.2 ));
 
-    // world.add(Rc::new(sphere(Point3::new(0.0, 0.0, -1.2), 0.5, mat_center)));
-    // world.add(Rc::new(sphere(Point3::new(0.0, -100.5, -1.0), 100.0, mat_ground)));
-    // world.add(Rc::new(sphere(Point3::new(-1.0, 0.0, -1.0), 0.5, mat_left)));
-    // world.add(Rc::new(sphere(Point3::new(-1.0, 0.0, -1.0), 0.4, mat_bubble)));
-    // world.add(Rc::new(sphere(Point3::new(1.0, 0.0, -1.0), 0.5, mat_right)));
+    world.add(Rc::new(sphere(Point3::new(0.0, 0.0, -1.2), 0.5, mat_center)));
+    world.add(Rc::new(sphere(Point3::new(0.0, -100.5, -1.0), 100.0, mat_ground)));
+    world.add(Rc::new(sphere(Point3::new(-1.0, 0.0, -1.0), 0.5, mat_left)));
+    world.add(Rc::new(sphere(Point3::new(-1.0, 0.0, -1.0), 0.4, mat_bubble)));
+    world.add(Rc::new(sphere(Point3::new(1.0, 0.0, -1.0), 0.5, mat_right)));
 
-    let radius = (PI/4.0).cos();
-    let mat_left = Rc::new(LambertianMaterial::new(Color::new(0.0, 0.0, 1.0)));
-    let mat_right = Rc::new(LambertianMaterial::new(Color::new(1.0, 0.0, 0.0)));
+    // let radius = (PI/4.0).cos();
+    // let mat_left = Rc::new(LambertianMaterial::new(Color::new(0.0, 0.0, 1.0)));
+    // let mat_right = Rc::new(LambertianMaterial::new(Color::new(1.0, 0.0, 0.0)));
     
-    world.add(Rc::new(sphere(Point3::new(-radius, 0.0, -1.0), radius, mat_left)));
-    world.add(Rc::new(sphere(Point3::new(radius, 0.0, -1.0), radius, mat_right)));
+    // world.add(Rc::new(sphere(Point3::new(-radius, 0.0, -1.0), radius, mat_left)));
+    // world.add(Rc::new(sphere(Point3::new(radius, 0.0, -1.0), radius, mat_right)));
 
     let mut cam: Camera = Camera::new();
 
-    cam.image_width = 500;
-    cam.samples_per_pixel = 50;
-    cam.max_child_rays = 25;
-    cam.vfov = 90;
-    
+    cam.image_width = 1080;
+    cam.samples_per_pixel = 250;
+    cam.max_child_rays = 50;
+    cam.vfov = 50;
+    cam.center = Point3::new(-2.0, 2.0, 1.0);
+    cam.look_at_point = Point3::new(0.0, 0.0, -1.0);
+    cam.vup = Dir3::Y;
+
     cam.render(&world);
     
 }
